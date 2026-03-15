4c095778bd6f723381e6a051a4f1bd8c8e9aef55
Mapax <Mar.zzzx1@gmail.com>
2026-03-15 16:59:35 +0500

    Добавил методыБ которые просил фронт

--- content/urls.py
+++ content/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('content/create/', views.PublicationContentCreateView.as_view(), name='publication_create'),
     path('content/update/<int:publication_id>/<int:id>/', views.PublicationContentUpdateView.as_view(), name='publication_detail'),
+    path('content/content_list/', views.PublicationContentList.as_view(), name='content_list'),
+    path('content/content/<int:id>/', views.PublicationContentGetById.as_view(), name='content_detail'),
+    path('content/delete_by_publication/<int:publication_id>', views.PublicationContentDeleteByPublicationId.as_view(), name='content_delete_by_publication'),
 ]
