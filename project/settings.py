03f17f57b414257e249720203d67b961294fbf4e
Сафонтьев Иван <safontev@bk.ru>
2026-09-03 16:18:44 +0500

    fix

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,19 +97,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080",
+    'http://localhost:8080',
+    'https://fund-meet-backend-wheat.vercel.app',
+    'https://*.vercel.app',
 ]
-
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True 
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
