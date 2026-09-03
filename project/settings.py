c675bb312d1aa4d98e43a4c0b99d049f3f4225d9
Сафонтьев Иван <safontev@bk.ru>
2026-09-03 16:50:12 +0500

    fix

--- project/settings.py
+++ project/settings.py
@@ -109,9 +109,15 @@ CSRF_TRUSTED_ORIGINS = [
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
+        conn_max_age=600
     )
+}
+
+DATABASES['default']['HOST'] = '2a05:d014:1577:8801:d832:412d:f798:9912'
+
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require',
+    'connect_timeout': 10,
 }
 
 
